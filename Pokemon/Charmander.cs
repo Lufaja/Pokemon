3a750bfba0b263eebea9464760a99adc06747272
Lukas van Hulst <Lukas.v.hulst@gmail.com>
2023-05-15 11:14:36 +0200

    assignment 1

---
+++ Pokemon/Charmander.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    public class Charmander
+    {
+        public string name;
+        public string type;
+        public string weakness;
+        public int hp;
+
+        public Charmander(string name, string type, string weakness, int hp)
+        {
+            this.name = name;
+            this.type = type;
+            this.weakness = weakness;
+            this.hp = hp;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public string GetType()
+        {
+            return type;
+        }
+        public string GetWeakness()
+        { return weakness; }
+
+        public int GetHp()
+        { return hp; }
+
+        public void BattleCry()
+        {
+            for (int i = 0; i <= 10; i++)
+            { Console.WriteLine(this.name); }
+        }
+    }
+}
---
+++ Pokemon/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool x = true;
+            while (x)
+            {
+                Console.WriteLine("Enter Charmander's name: ");
+                string char_name = Console.ReadLine();
+                Charmander charmander = new Charmander(char_name, "fire", "water", 100);
+                Console.WriteLine("Name: " + charmander.GetName());
+                Console.WriteLine("Type: " + charmander.GetType());
+                Console.WriteLine("Weakness: " + charmander.GetWeakness());
+                Console.WriteLine("Hp: " + charmander.GetHp());
+                for (int i = 0; i <= 10; i++)
+                { Console.WriteLine(char_name); }
+                Console.WriteLine("Do you want to continue y/n: ");
+                string check_state = Console.ReadLine().ToLower();
+                if (check_state == "y")
+                {
+                    x = true;
+                }
+                else
+                {
+                    x = false; break;
+                }
+            }
+        }
+
+    }
+}
